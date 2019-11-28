dd1323179be607167bdcfe056a003c0ab2c0b343
lijiapeng <806626194@qq.com>
2019-11-28 09:43:22 +0800

    first commit

---
+++ mall-web/src/main/java/com/xyhs/b2c/MallWebApplication.java
@@ -0,0 +1,16 @@
+package com.xyhs.b2c;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ljp
+ * @apiNote
+ * @date 9:01 2019/11/28
+ **/
+@SpringBootApplication
+public class MallWebApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MallWebApplication.class,args);
+    }
+}
